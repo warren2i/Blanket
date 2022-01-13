39f1fb674349bf446c40e511bd0578f257cd3462
warren2i <83583153+Tensorshark@users.noreply.github.com>
2022-01-13 18:47:09 +0000

    Init

--- Blanket.py
+++ Blanket.py
@@ -5,15 +5,32 @@ import io
 findvar = re.compile(r'(?<=\$)(.*?)(?=[\s,),\.,=,\",\;])') ## lockback search for $ look forward search for end char or space
 vararray = []
 randomvararray=[]
-f = open("scripts/newshell1.txt", "rt")
+f = open("scripts/shell.ps1", "rt")
 lines = f.readlines()
 f.close()
-f = open("scripts/newshell1.txt", "rt")
+f = open("scripts/shell.ps1", "rt")
 
 w = open("scripts/newshell.txt", "wt")
 script = f.read()
 f.close()
-
+list =[
+	"System.Net.Sockets.TcpClient",
+    "System.IO.StreamWriter",
+	"System.Byte",
+	"System.Text.Encoding",
+	"System.Text.AsciiEncoding",
+	"System.Diagnostics.Process",
+	"System.Text.ASCIIEncoding",
+	"Net.Sockets.TCPClient",
+	"System.Net.Networkinformation.Ping",
+	"PSObject",
+	"Net.WebClient",
+	"System.Net.HttpListener",
+	"Security.Principal.WindowsPrincipal",
+	"System.Net.IPEndPoint",
+	"TExT.AscIIENcoDINg",
+	"io.strEamWritEr"
+]
 def whitespace():
     ##this function will add random amounts of whitespace at the start of each line
     pass
@@ -39,22 +56,50 @@ def randomvar(length): ##create a random variable of len length
     randomvar=(''.join(random.choice(letters) for i in range(length)))  ##creates a random string of length randomlen
     return randomvar
 
+def breakdatatype(string,rando):
+    breakdataarr=[]
+    n = random.randrange(3,7) # chunk length
+    chunks = [string[i:i+n] for i in range(0, len(string), n)]
+    for x in enumerate(chunks):
+        breakdataarr.append(['$'+randomvar(rando),x[1]])
+    newstring=("".join('%s'%item[0] for item in breakdataarr))
+
+    return breakdataarr,newstring
+
 for line in lines: ## itterate line by line
     lines = findvar.finditer(line) ##match variables
     for var in lines: ## loop over variables
-        print(var)
+        #print(var)
         var = var.group(0) ## pass index of the group, strips what we dont want.
         if var not in vararray: ## don't populate array with same value twice
             vararray.append('$'+var) ## add found var to vararray
             randomvararray.append('$'+randomvar(10)) ## add random var to randomvar array at matching index
 
-for x in enumerate(vararray): ## replace variables with random variables
-    script = script.replace(vararray[x[0]], randomvararray[x[0]])
-
+    for x in enumerate(vararray): ## replace variables with random variables
+        script = script.replace(vararray[x[0]], randomvararray[x[0]])
+varr=[]
 s = io.StringIO(script) ## used to itterate string 'line by line...'
+for line in s:
+    print(line)
+    for l in list:
+        if l in line:
+            b = breakdatatype(l, random.randrange(3, 14))  ## pass string and random length to breakdata function
+            # print(line)
+            line = line.replace(l, b[1])
+            # print(line)
+            # print((b[1])+l) ##just to debug
+            for a in enumerate(b[0]):
+                varr.append((b[0][a[0]][0]) + ' = "' + b[0][a[0]][
+                    1] + '"')  ##formats into powershell variable format ie $MUI = "Syst"
+    script = script + line
+s = io.StringIO(script) ## used to itterate string 'line by line...'
+
+w.write("\n".join(varr))
+w.write("\n")
 for line in s:
     w.write(line) ## write line to file
     w.write(addcomments() + '\n') ## write comment line to file
+
 
 
 
